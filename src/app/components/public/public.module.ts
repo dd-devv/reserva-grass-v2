ecde3d2389a6b5c029186b5bc1b0c9340e73d5fd
Dino Roca <rocalaurented@gmail.com>
2024-10-17 20:55:22 -0500

    Estructura de public

--- src/app/components/public/public.module.ts
+++ src/app/components/public/public.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
-import { PagesComponent } from './pages/pages.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    PagesComponent,
     HomeComponent,
     ContactComponent
   ],
