fe33536de8d693dc46747e1e28fb2f8363c66753
Dino Roca <rocalaurented@gmail.com>
2024-10-18 22:36:14 -0500

    Estructuracion de modulo auth

--- src/app/components/auth/auth-routing.module.ts
+++ src/app/components/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { RegistroEmpresaComponent } from './components/registro-empresa/registro-empresa.component';
+import { ContainerComponent } from './container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContainerComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'registro', component: RegistroComponent },
+      { path: 'registro-empresa', component: RegistroEmpresaComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empresa.component';
+import { ContainerComponent } from './container/container.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { RegistroEmpresaComponent } from './components/registro-empresa/registro-empresa.component';
 
 
 @NgModule({
   declarations: [
-
     LoginComponent,
     RegistroComponent,
-    RegistroEmpresaComponent
+    RegistroEmpresaComponent,
+    ContainerComponent
   ],
   imports: [
     CommonModule,
