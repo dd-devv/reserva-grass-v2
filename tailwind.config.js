a3d41c41431527a6c1983f16c070807bacb79fa6
Mirko <giancarlo1234522@gmail.com>
2024-11-01 15:29:37 -0500

    style horario dinamico

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       /* colors: {
@@ -148,22 +147,5 @@ module.exports = {
       // },
     },
   },
-<<<<<<< Updated upstream
-  plugins: [
-    require('flowbite/plugin') 
-  ],
-
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "light", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
-=======
   plugins: [require("flowbite/plugin")],
->>>>>>> Stashed changes
 };
