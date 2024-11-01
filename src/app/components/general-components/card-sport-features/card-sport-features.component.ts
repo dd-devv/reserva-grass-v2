5ac78eaa8a4a1aafdea8bb2ad0c819a2de1cbbb1
Dino Roca <rocalaurented@gmail.com>
2024-11-01 15:07:15 -0500

    Cambio de colores segun el tipo de cancha

--- src/app/components/general-components/card-sport-features/card-sport-features.component.ts
+++ src/app/components/general-components/card-sport-features/card-sport-features.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-sport-features',
   templateUrl: './card-sport-features.component.html',
   styleUrl: './card-sport-features.component.css'
 })
-export class CardSportFeaturesComponent {
+export class CardSportFeaturesComponent implements OnInit {
+  @Input() cancha: any;
+  @Output() tipo_cancha = new EventEmitter<string>();
 
-  isHovered = false;
+  public tipo = '';
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSelect(tipo: string): void {
+    this.tipo = tipo;
+    this.tipo_cancha.emit(this.tipo);
+  }
 }
