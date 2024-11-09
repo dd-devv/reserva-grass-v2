a033d9a5d3cb6e9baeaf97b74ba573b3c126ad2e
Dino Roca <rocalaurented@gmail.com>
2024-11-09 11:40:53 -0500

    Estructuracion de lado grass

--- src/app/components/grass/grass-routing.module.ts
+++ src/app/components/grass/grass-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CanchasComponent } from './pages/canchas/canchas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContainerComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'canchas', component: CanchasComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/grass/grass.module.ts
+++ src/app/components/grass/grass.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GrassRoutingModule } from './grass-routing.module';
+import { ContainerComponent } from './container/container.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CanchasComponent } from './pages/canchas/canchas.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContainerComponent,
+    HomeComponent,
+    CanchasComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule,
     GrassRoutingModule
