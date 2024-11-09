e7933b9a77f9c07887b3c938bf66f648cba7bbd9
Alex Martinez <martinez246alex@gmail.com>
2024-11-09 09:16:25 -0500

    "add calendar and reestructure ver canchas"

--- src/app/components/general-components/card-horario/card-horario.component.ts
+++ src/app/components/general-components/card-horario/card-horario.component.ts
@@ -1,12 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-card-horario',
   templateUrl: './card-horario.component.html',
   styleUrl: './card-horario.component.css'
 })
-export class CardHorarioComponent {
+export class CardHorarioComponent implements OnInit {
 
   @Input() tipo!: string;
 
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
