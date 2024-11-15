f85a2cc7206e9e14976799e43346872c4b62ab33
Dino Roca <rocalaurented@gmail.com>
2024-11-14 22:08:25 -0500

    Cerrar sesion

--- src/app/components/user/pages/perfil/sidebar/sidebar.component.ts
+++ src/app/components/user/pages/perfil/sidebar/sidebar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../../services/auth.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
+  constructor(
+    private _authService: AuthService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  logout() {
+    this._authService.logout();
+    location.reload();
+  }
 }
