d8ea82c4b33572c36a5c5d3e5d81fd10700aeff0
Dino Roca <rocalaurented@gmail.com>
2024-11-18 21:48:28 -0500

    Estructira inicial de grass

--- src/app/components/user/pages/perfil/portrait/portrait.component.ts
+++ src/app/components/user/pages/perfil/portrait/portrait.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portrait',
   templateUrl: './portrait.component.html',
   styleUrl: './portrait.component.css'
 })
-export class PortraitComponent {
+export class PortraitComponent implements OnInit {
+
+  public user_lc: any = {};
+
+  ngOnInit(): void {
+    this.user_lc = JSON.parse(localStorage.getItem('user_data')!);
+  }
 
 }
