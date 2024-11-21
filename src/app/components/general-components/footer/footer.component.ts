181c27ece1183da20f8036cfbdbef58133207020
EmirAve <105077208+EmirAve@users.noreply.github.com>
2024-11-20 23:54:55 -0500

    footer

--- src/app/components/general-components/footer/footer.component.ts
+++ src/app/components/general-components/footer/footer.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  // Control de visibilidad de las secciones
+  isInfoOpen = false;
+  isSocialOpen = false;
 
+  // Función para alternar el estado de las secciones
+  toggleSection(section: string) {
+    if (section === 'info') {
+      this.isInfoOpen = !this.isInfoOpen;
+    } else if (section === 'social') {
+      this.isSocialOpen = !this.isSocialOpen;
+    }
+  }
+
+  // Referencia al div que se va a animar
+  @ViewChild('animarDiv') animarDiv!: ElementRef;
+
+  ngAfterViewInit() {
+    this.iniciarObservador();
+  }
+
+  // Función para iniciar el observador de intersección
+  iniciarObservador() {
+    // Asegurarse de que el div existe
+    if (!this.animarDiv) return;
+
+    const divElement = this.animarDiv.nativeElement;
+
+    // Crear un observador de intersección
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Cuando el div entra en el viewport, aplica la clase para animarlo
+          divElement.classList.add('translate-x-0');
+        } else {
+          // Si el div ya no está visible, mueve el div fuera de la pantalla
+          divElement.classList.remove('translate-x-0');
+          divElement.classList.add('translate-x-[-100%]');
+        }
+      });
+    }, { threshold: 0.5 }); // Se activa cuando el 50% del div es visible
+
+    // Empieza a observar el div
+    observer.observe(divElement);
+  }
 }
