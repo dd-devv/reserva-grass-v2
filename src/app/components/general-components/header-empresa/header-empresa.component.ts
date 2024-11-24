ab646b3ebb97e91aee7f9dc74c75ffef02b0e534
Dino Roca <rocalaurented@gmail.com>
2024-11-23 23:17:03 -0500

    Cerrar sesion de lado empresa

--- src/app/components/general-components/header-empresa/header-empresa.component.ts
+++ src/app/components/general-components/header-empresa/header-empresa.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header-empresa',
@@ -8,8 +9,18 @@ import { initFlowbite } from 'flowbite';
 })
 export class HeaderEmpresaComponent implements OnInit{
 
+  constructor(
+      private _authService: AuthService
+    ) {
+  
+    }
+
   ngOnInit(): void {
     initFlowbite();
   }
 
+  logout() {
+    this._authService.logout();
+    location.reload();
+  }
 }
