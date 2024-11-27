8e9bf9b93b0f95eeb0ef4c09577a4d7ef9a81c82
Dino Roca <rocalaurented@gmail.com>
2024-11-27 18:59:55 -0500

    Contactos registrado en la base de datos

--- src/app/components/public/pages/contact/contact.component.ts
+++ src/app/components/public/pages/contact/contact.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../../../services/user.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  public contacto: any = {};
+  public load_btn = false;
+
+  constructor(
+    private _title: Title,
+    private _toastrService: ToastrService,
+    private _userService: UserService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this._title.setTitle('Contacto');
+  }
+
+  registro(registroForm: any) {
+    if(registroForm.valid) {
+      this.load_btn = true;
+      this._userService.enviar_mensaje_contacto(this.contacto).subscribe(
+        response =>{
+          this._toastrService.success('Se envió correctamente el mensaje', 'ENVIADO!');
+
+          this.contacto = {};
+          this.load_btn = false;
+        }
+      );
+    } else {
+      this._toastrService.error('No se envió, error!', 'ERROR!');
+    }
+  }
 
 }
