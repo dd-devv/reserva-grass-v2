4df1e7ed1b7b288c41fec8e51a2b079a61990ec8
Dino Roca <rocalaurented@gmail.com>
2024-11-28 20:28:39 -0500

    Politicas de privacidad y estructura de preguntas frecuentes

--- src/app/components/grass/pages/ayuda/ayuda.component.ts
+++ src/app/components/grass/pages/ayuda/ayuda.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-ayuda',
   templateUrl: './ayuda.component.html',
   styleUrl: './ayuda.component.css'
 })
-export class AyudaComponent {
+export class AyudaComponent implements OnInit {
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
 }
