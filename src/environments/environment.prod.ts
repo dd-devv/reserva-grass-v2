038f878b0b1f3b74c62bb046c6f78c44a723bcf8
Dino Roca <rocalaurented@gmail.com>
2025-03-03 22:47:40 -0500

    Feat: Nueva url de la api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     googleMapsApiKey: 'AIzaSyC21A6bPPc14Vl3YdUxDVBpCJ8d_Y_cTyo',
-    url: 'https://api.reservatugrass.com/api/',
-    url_socket: 'https://api.reservatugrass.com',
+    url: 'https://api.tugrass.com/api/',
+    url_socket: 'https://api.tugrass.com',
     socketPath: '/socket.io'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyC21A6bPPc14Vl3YdUxDVBpCJ8d_Y_cTyo',
-  url: 'https://api.reservatugrass.com/dev/api/',
-  url_socket: 'https://api.reservatugrass.com',
+  url: 'https://api.tugrass.com/dev/api/',
+  url_socket: 'https://api.tugrass.com',
   socketPath: '/dev/socket.io'
 }
